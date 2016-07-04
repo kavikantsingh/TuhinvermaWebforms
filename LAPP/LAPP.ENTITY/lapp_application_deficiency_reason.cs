0dda39bd5c625135c84be245356b7a671af93182
shekhar_babu <shekhar.babu@eduloka.com>
2016-07-04 18:46:12 +0530

    Error Fixes.

--- LAPP/LAPP.ENTITY/lapp_application_deficiency_reason.cs
+++ LAPP/LAPP.ENTITY/lapp_application_deficiency_reason.cs
@@ -19,5 +19,8 @@ namespace LAPP.ENTITY
         public int Modified_By { get; set; }
         public DateTime DTS { get; set; }
         public string Name { get; set; }
+        public DateTime End_Date { get; set; }
+        public bool Additional_Text { get; set; }
+        public int Deficiency_Template_ID { get; set; }
     }
 }
